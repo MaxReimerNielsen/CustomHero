fd308ddd92b660d52936b512d266c121a2df85ca
dk\mrn <mrn@sitecore.net>
2014-11-28 12:38:01 +0100

    Fixed sprite sheet

--- CustomHero/scripts/customhero.js
+++ CustomHero/scripts/customhero.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function() {
-  var $heroes = $('#heroes');
+  var heroes,
+    $heroes = $('#heroes');
 
   $heroes.sortable({
     placeholder: "ui-state-placeholder",
@@ -10,31 +11,40 @@
 
   var initHeroes = function () {
     $.getJSON('/data/heroes.json', function (data) {
-      createThumbnails(data);
+      heroes = data.heroes;
+
+      createThumbnails(sortHeroes());
     });
   };
 
-  var createThumbnails = function (data) {
+  var sortHeroes = function(sortBy) {
+    sortBy = sortBy || "name";
+
+    heroes = _.sortBy(heroes, function (hero) {
+      return hero[sortBy];
+    });
+  };
+
+  var createThumbnails = function () {
     for (var i = 0; i < 115; i++) {
       var $listItem;
 
-      if (i < data.heroes.length) {
-        var hero = data.heroes[i],
+      if (i < heroes.length) {
+        var hero = heroes[i],
           name = hero.name,
           localizedName = hero.localized_name,
           id = hero.id,
-          $span = $('<span />').text(localizedName),
-          bgPadding = 5,
-          spriteWidth = 81,
-          spriteHeight = 104,
+          spriteWidth = 71,
+          spriteHeight = 94,
           imagePerRow = 12,
-          leftPos = -(((id % imagePerRow) - 1) * spriteWidth) - bgPadding,
-          topPos = -(Math.floor(id / imagePerRow) * spriteHeight) - bgPadding,
+          row = Math.floor((id - 1) / imagePerRow),
+          column = (id - 1) % imagePerRow,
+          leftPos = -(column * spriteWidth),
+          topPos = -(row * spriteHeight),
           bgPosition = leftPos + 'px ' + topPos + 'px';
 
-        console.log(id % 12);
-
         $listItem = $('<li style="background-position: ' + bgPosition + ';" />').attr('data-hero-id', id).attr('data-hero-name', name);
+        var $span = $('<span />').html(localizedName);
         $span.appendTo($listItem);
       } else {
         $listItem = $('<li style="background-image: none;" />');
